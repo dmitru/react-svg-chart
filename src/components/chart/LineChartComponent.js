d14cf067247ea506cd8e4635b4e417949d48ba33
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-02-18 19:12:00 +0000

    Minor refactoring

--- src/components/chart/LineChartComponent.js
+++ src/components/chart/LineChartComponent.js
@@ -8,19 +8,19 @@ class LineChart extends React.Component {
   render() {
     const { data, xAxis, yAxis } = this.props;
     const segments = [];
+    const points = [];
     for (let i = 1; i < data.length; ++i) {
-      segments.push({
-        x1: xAxis.scale(data[i - 1].x),
-        x2: xAxis.scale(data[i].x),
-        y1: yAxis.scale(data[i - 1].y),
-        y2: yAxis.scale(data[i].y)
-      });
+      const x1 = xAxis.scale(data[i - 1].x);
+      const x2 = xAxis.scale(data[i].x);
+      const y1 = yAxis.scale(data[i - 1].y);
+      const y2 = yAxis.scale(data[i].y);
+      segments.push(
+        <line key={x1.valueOf()} x1={x1} x2={x2} y1={y1} y2={y2} />
+      );
     }
     return (
       <g className="line-chart">
-        {segments.map((s) => (
-          <line key={s.x1.valueOf()} x1={s.x1} x2={s.x2} y1={s.y1} y2={s.y2} />
-        ))}
+        {segments}
       </g>
     );
   }
